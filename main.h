d4f559b22c1a155c4565206e6c8ffef7189907da
Jaredglass3 <Jaredglass352@gmail.com>
2022-10-31 11:43:18 -0700

    Removed old files and restarted, Subbmitted new Header file

--- main.h
+++ main.h
@@ -3,22 +3,27 @@
 #include <stdarg.h>
 
 /**
-* struct print - struct for conversion specifiers
-* @print: struct print
-* @p: print function specified
+* struct specifiers - Struct specifiers
+* @specifier: The conversion specifier
+* @f: The function pointer
 */
-typedef struct print
+typedef struct specifiers
 {
-char *print;
-int (*p)();
-} print_t;
+char *specifier;
+int (*f)(va_list args);
+} spc_dt;
 
+int _write(char c);
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
 
-int p_char(va_list arg);
-int p_str(va_list arg);
-int p_dec(va_list arg);
-int p_int(va_list arg);
-
-#endif /* MAIN_H */
+#endif /* _HOLBERTON */
